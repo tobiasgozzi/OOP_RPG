6f1bd41c379b24e25cce1baed0eb3d7108f7b261
Tobias Gozzi <tobias.gozzi@me.com>
2016-04-18 18:24:47 +0200

    Initial Commit

---
+++ OOP-exerciseTests/OOP_exerciseTests.swift
@@ -0,0 +1,36 @@
+//
+//  OOP_exerciseTests.swift
+//  OOP-exerciseTests
+//
+//  Created by Tobias Gozzi on 18.04.16.
+//  Copyright © 2016 Tobias Gozzi. All rights reserved.
+//
+
+import XCTest
+@testable import OOP_exercise
+
+class OOP_exerciseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
